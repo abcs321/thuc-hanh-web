ce6ba0680683dfa0266aae3b77a68cac1d91a1d1
abcs321 <zerovndragon@gmail.com>
2026-08-10 17:01:51 +0700

    Them trang about.php gioi thieu ban than

---
+++ about.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Giới thiệu bản thân</title>
+</head>
+<body>
+    <h1>Xin chào, tôi là Trần Minh Tú </h1>
+    <p>Sinh viên TRƯỜNG ĐẠI HỌC THỦ ĐÔ HÀ NỘI, đang học môn Lập trình Web.</p>
+
+    <h2>Các dự án đang làm</h2>
+    <ul>
+        <li>BTL thư viện mini</li>
+    </ul>
+</body>
+</html>
